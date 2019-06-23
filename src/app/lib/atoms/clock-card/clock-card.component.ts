faa4a72c422f79bcc173efb9b6cce785a1d9fdda
gurezo <akihiko.kigure@gmail.com>
2019-06-23 15:55:47 +0900

    tsconfig update

--- src/app/lib/atoms/clock-card/clock-card.component.ts
+++ src/app/lib/atoms/clock-card/clock-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-clock-card',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./clock-card.component.scss']
 })
 export class ClockCardComponent implements OnInit {
+
+  @Input() value: number;
 
   constructor() { }
 
