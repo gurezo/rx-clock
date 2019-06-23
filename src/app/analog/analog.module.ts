18d90b9442156677e48d09e84f0d3f1199eecd2c
gurezo <akihiko.kigure@gmail.com>
2019-06-23 15:02:38 +0900

    generate module compoenent

---
+++ src/app/analog/analog.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AnalogRoutingModule } from './analog-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AnalogRoutingModule
+  ]
+})
+export class AnalogModule { }
---
+++ src/app/digital/digital.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DigitalRoutingModule } from './digital-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    DigitalRoutingModule
+  ]
+})
+export class DigitalModule { }
